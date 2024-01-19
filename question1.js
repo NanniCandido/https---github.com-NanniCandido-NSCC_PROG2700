e4de3dbaec2332de580df4d7c3cb0467f3f0ffe3
Elaine Candido da Silva <nannicandido@gmail.com>
2024-01-19 10:58:03 -0400

    Finished question 1

--- question1.js
+++ question1.js
@@ -8,30 +8,26 @@ Description: Write a program that get a string and do the follow:
         remove the first and the last letters
 */
 
-let myString = 'Elaine';
-let stringSize = myString.length;
-let myCounter = stringSize - 1;
-let tempLetter;
-let newString = [];
+// Variables declaration
+let myString = 'Elaina';  //My string
+let stringSize = myString.length;  // getting the string size
+let tempString = ""; // variable to store the string after processing
 
-console.log(myString);
-
+// Processing and Calculation
 let firstLetter = myString[0];  // get the first letter of the string
 let lastLetter = myString[stringSize-1]; // get the last letter of the string
 
 if (firstLetter.toLowerCase() == lastLetter.toLowerCase()) 
 {
-    // Inicialize counter at the last caracter
-    for (let i=stringSize-1;i<0;i--) {
-        newString[myCounter] = myString[i];
-        myCounter--;
+    // Loop to replace the position of the letter in the string
+    for (let i=stringSize-1;i>=0;i--) {
+        tempString += myString[i];
     } 
 
-    console.log(firstLetter);
-    console.log(lastLetter);
-    console.log(myString);
-    console.log(newString);
-
 } else {
-    console.log(myString.slice(1, -1));
+    // removing the first and last letters when they are equals
+    tempString = myString.slice(1, -1);
 }
+// Output
+console.log("My string before running the program: "+myString);
+console.log("My string after running the program: "+tempString);
